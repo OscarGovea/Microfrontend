837ed0e7c2e62a46058dd2d9c8dd7074b48365ae
Oscar Daniel Govea Garcia <oscar.govea@ksp.com.mx>
2024-04-08 16:11:29 -0600

    Reestructurar carpetas y creacion del proyecto 03

---
+++ 03-colors-app/host/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ColorPicker from "colorPicker/ColorPicker"
+
+
+const App = () => (
+  <>
+    <ColorPicker/>
+  </>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
